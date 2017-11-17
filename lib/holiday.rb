e267bbb4970a642b8113e820620b099de6815033
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-17 20:15:57 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -76,7 +76,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
 output= ""
   holiday_hash.each do |season, holiday|
-    output << season  ":"  "\n"  "\t"
+    output << (season  ":"  "\n"  "\t")
     holiday.each do |holiday,supplies|
       output << holiday + ": "
       counter=0
